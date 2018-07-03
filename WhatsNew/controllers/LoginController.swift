5e12f6cd2f4e18ba38b64fc85ea5f0979a3c5e62
Unknown <candice.guitton@hotmail.fr>
2018-07-03 23:46:02 +0200

    add loginController

--- WhatsNew/controllers/LoginController.swift
+++ WhatsNew/controllers/LoginController.swift
@@ -7,17 +7,34 @@
 //
 
 import UIKit
+import GoogleSignIn
 
-class ViewController: UIViewController {
-
+class LoginController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate{
+    
+    @IBOutlet weak var googleButton: GIDSignInButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().delegate = self
+        
+        // Uncomment to automatically sign in the user.
+        //GIDSignIn.sharedInstance().signInSilently()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if error != nil {
+            print(error)
+            return
+        }
+        
+        print(user.profile.familyName)
     }
 
 
